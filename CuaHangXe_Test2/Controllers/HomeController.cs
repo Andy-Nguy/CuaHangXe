99af83c03691d65baa0e4ca56203cc8bca23e4e3
thinhnh <nthhh296@gmail.com>
2024-11-10 20:04:47 +0700

    update filter in SanPham and font. Adjust View

--- CuaHangXe_Test2/Controllers/HomeController.cs
+++ CuaHangXe_Test2/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace CuaHangXe_Test2.Controllers
     public class HomeController : Controller
     {
         //Hi
-        SieuXeDbEntities1 db =new SieuXeDbEntities1();
+        SieuXeDbEntities1 db = new SieuXeDbEntities1();
         // GET: Home
         public ActionResult Index(string HangXe = "Lamborghini", string DongXe = "Hypercar")
         {  /* / Lọc theo hãng xe*/
@@ -21,14 +21,36 @@ namespace CuaHangXe_Test2.Controllers
             ViewBag.dsXeTheoDong = dsXeTheoDong;
             return View();
         }
-        public ActionResult SanPham()
+
+        public ActionResult SanPham(string dongXe = null, List<string> mauXe = null)
         {
             // Lấy toàn bộ sản phẩm từ cơ sở dữ liệu
             List<Xe> allProducts = db.Xes.ToList();
-            ViewBag.AllProducts = allProducts;
 
-            return View();
+            // Lấy danh sách các màu sắc xe và các dòng xe
+            List<string> colors = db.Xes.Select(x => x.MauSac).Distinct().ToList();
+            ViewBag.ColorsOfProduct = colors ?? new List<string>();
+
+            List<string> dongXes = db.Xes.Select(x => x.DongXe).Distinct().ToList();
+            ViewBag.CacDongXe = dongXes ?? new List<string>();
+
+            // Lọc theo dòng xe nếu có
+            if (!string.IsNullOrEmpty(dongXe))
+            {
+                allProducts = allProducts.Where(row => row.DongXe == dongXe).ToList();
+            }
+
+            // Lọc theo màu sắc nếu có
+            if (mauXe != null && mauXe.Any())
+            {
+                allProducts = allProducts.Where(row => mauXe.Contains(row.MauSac)).ToList();
+            }
+
+            return View(allProducts);
         }
+
+
+
         public ActionResult ChiTietSanPham(string id)
         {
             //string id = "XE001";
@@ -41,7 +63,5 @@ namespace CuaHangXe_Test2.Controllers
 
             return View(xeChiTiet);
         }
-
-
     }
 }
