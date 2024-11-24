7f98faed6024b40539149d6c9bc1f356d7f0f9b0
Andy-Nguy <duyanh05012004@gmail.com>
2024-11-24 11:02:40 +0700

    t mới thêm cái trang HopDong khi mà nhấn vào nút Mua á nha

--- CuaHangXe_Test2/Controllers/HomeController.cs
+++ CuaHangXe_Test2/Controllers/HomeController.cs
@@ -62,5 +62,71 @@ namespace CuaHangXe_Test2.Controllers
 
             return View(xeChiTiet);
         }
+        
+        public ActionResult NhapThongTinHopDong(string maXe)
+        {
+            // Lấy thông tin xe từ cơ sở dữ liệu nếu cần
+            var xe = db.Xes.FirstOrDefault(x => x.MaXe == maXe);
+
+            // Tạo MaKhachHang và MaHopDong tự động
+            string maKhachHang = GenerateMaKhachHang(); // Hàm tạo mã khách hàng
+            string maHopDong = GenerateMaHopDong(); // Hàm tạo mã hợp đồng
+
+            //
+            decimal giaBanXe = xe?.GiaBanXe ?? 0;
+
+            // Tính thuế VAT (10% giá trị bán của xe)
+            decimal thueVAT = giaBanXe * 0.1m;
+
+            // Tính thuế nhập khẩu (70% giá trị bán của xe)
+            decimal thueNhapKhau = giaBanXe * 0.7m;
+
+            // Tính thuế TTĐB (60% giá trị bán của xe)
+            decimal thueTTDB = giaBanXe * 0.6m;
+
+            // Tính phí trước bạ (10% giá trị xe)
+            decimal phiTruocBa = giaBanXe * 0.1m;
+
+            // Tính tổng giá trị hợp đồng
+            decimal tongGiaTriHopDong = giaBanXe + thueVAT + thueNhapKhau + thueTTDB + phiTruocBa;
+
+
+            // Khởi tạo đối tượng hợp đồng
+            var hopDong = new HopDongMuaBan
+            {
+                MaKhachHang = maKhachHang,
+                MaXe = maXe,
+                MaHopDong = maHopDong,
+                NgayLapHopDong = DateTime.Now,
+                TongGiaTriHopDong =tongGiaTriHopDong
+            };
+            ViewBag.TenXe = xe?.TenXe;
+            ViewBag.MauSac = xe?.MauSac;
+            ViewBag.SoKhung = xe?.SoKhungXe;
+            ViewBag.GiaBan=xe?.GiaBanXe;
+
+            return View(hopDong);
+        }
+
+        // Hàm tạo mã khách hàng
+        private string GenerateMaKhachHang()
+        {
+            // Tạo số ngẫu nhiên từ 10000 đến 99999 (5 chữ số)
+            Random random = new Random();
+            int randomNumber = random.Next(10000, 100000); // Số ngẫu nhiên từ 10000 đến 99999
+            return "KH" + randomNumber.ToString(); // KH + số ngẫu nhiên
+        }
+
+        // Hàm tạo mã hợp đồng (Sử dụng số ngẫu nhiên 5 chữ số)
+        private string GenerateMaHopDong()
+        {
+            // Tạo số ngẫu nhiên từ 10000 đến 99999 (5 chữ số)
+            Random random = new Random();
+            int randomNumber = random.Next(10000, 100000); // Số ngẫu nhiên từ 10000 đến 99999
+            return "HD" + randomNumber.ToString(); // HD + số ngẫu nhiên
+        }
+
+
+        ////
     }
 }
