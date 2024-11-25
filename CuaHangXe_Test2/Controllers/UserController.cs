38891d9014f5281b10184b016765829416e89dd0
thinhnh <nthhh296@gmail.com>
2024-11-25 23:50:32 +0700

    Add login, register

--- CuaHangXe_Test2/Controllers/UserController.cs
+++ CuaHangXe_Test2/Controllers/UserController.cs
@@ -13,5 +13,85 @@ namespace CuaHangXe_Test2.Controllers
         {
             return View();
         }
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(NhanVien nv)
+        {
+            if (nv != null)
+            {
+                SieuXeDbEntities3 db = new SieuXeDbEntities3();
+                NhanVien nvCheck = db.NhanViens.Where(nvien => nvien.TenDangNhap == nv.TenDangNhap).FirstOrDefault();
+                if (nvCheck != null)
+                {
+                    if (nvCheck.MatKhau == nv.MatKhau)
+                    {
+                        HttpCookie authCookie = new HttpCookie("auth", nvCheck.TenDangNhap);
+                        HttpCookie nameCookie = new HttpCookie("name", HttpUtility.UrlEncode(nvCheck.TenNhanVien, System.Text.Encoding.UTF8));
+                        Response.Cookies.Add(nameCookie);
+                        HttpCookie roleCookie = new HttpCookie("role", nvCheck.ChucVuNhanVien);
+                        Response.Cookies.Add(authCookie);
+                        Response.Cookies.Add(nameCookie);
+                        Response.Cookies.Add(roleCookie);
+
+                        return RedirectToAction("Index", "Home");
+                    }
+                }
+            }
+            //ModelState.AddModelError("MatKhau", "Đăng nhập không thành công !");
+            return View();
+        }
+        public ActionResult Logout()
+        {
+            HttpCookie authCookie = new HttpCookie("auth");
+            HttpCookie nameCookie = new HttpCookie("name");
+            Response.Cookies.Add(nameCookie);
+            HttpCookie roleCookie = new HttpCookie("role");
+
+            authCookie.Expires = DateTime.Now.AddDays(-1);
+            Response.Cookies.Add(authCookie);
+
+            nameCookie.Expires = DateTime.Now.AddDays(-1);
+            Response.Cookies.Add(nameCookie);
+
+            roleCookie.Expires = DateTime.Now.AddDays(-1);
+            Response.Cookies.Add(nameCookie);
+
+            return Redirect("/");
+        }
+        public ActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Register(NhanVien nv)
+        {
+            if (nv != null)
+            {
+                SieuXeDbEntities3 db = new SieuXeDbEntities3();
+                NhanVien nvCheck = db.NhanViens.Where(nvien=>nvien.TenDangNhap == nv.TenDangNhap).FirstOrDefault();
+                if (nvCheck == null)
+                {
+                    if (nv.ChucVuNhanVien.Equals("Quản lý"))
+                        nv.VaiTro = "admin";
+                    else
+                        nv.VaiTro = "user";
+                    nv.MaNhanVien = TaoMaNV();
+                    db.NhanViens.Add(nv);
+                    db.SaveChanges();
+                    return RedirectToAction("Login", "User");
+                }
+            }
+            return View();
+        }
+        public string TaoMaNV()
+        {
+            SieuXeDbEntities3 db = new SieuXeDbEntities3();
+            int soNVHienTai = db.NhanViens.ToList().Count;
+            soNVHienTai++;
+            return "NV" + soNVHienTai.ToString("D3");
+        }
     }
 }
